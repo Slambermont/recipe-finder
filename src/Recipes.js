5ab683e58a6bf354eb455836c150d84e3a215c1d
Salome Lambermont <salome.lambermont@gmail.com>
2018-09-18 20:05:43 +0100

    Add error handling on API call

--- src/Recipes.js
+++ src/Recipes.js
@@ -10,7 +10,8 @@ class Recipes extends Component {
 
     this.state = {
       hits: [],
-      isLoading: false
+      isLoading: false,
+      error: null,
     };
   }
 
@@ -18,12 +19,23 @@ class Recipes extends Component {
     this.setState({ isLoading: true });
 
     fetch(API + '&q=chicken')
-      .then(response => response.json())
-      .then(data => this.setState({ hits: data.hits, isLoading: false }));
+      .then(response => {
+        if (response.ok) {
+          return response.json()
+        } else {
+          throw new Error('Something went wrong!');
+        }
+      })
+      .then(data => this.setState({ hits: data.hits, isLoading: false }))
+      .catch(error => this.setState({ error, isLoading: false}))
   }
 
   render() {
     const hits = this.state.hits;
+
+    if ( error ) {
+      return <p>{this.state.error.message}</p>;
+    }
 
     if ( this.state.isLoading ) {
       return <p>Loading...</p>;
